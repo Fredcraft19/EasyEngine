f79f5f9fa3836e18d7daef6b046ea16b65412121
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-01 23:37:18 +0100

    Update code.js

--- EasyEngine/code.js
+++ EasyEngine/code.js
@@ -1,6 +1,9 @@
 // This is essential as it is when, well. The engine is loaded.
 // If you try to run any engine related code before its loaded, errors may occur.
 
+// This is essential as it is when, well. The engine is loaded.
+// If you try to run any engine related code before its loaded, errors may occur.
+
 
 window.addEventListener("engine-loaded", () => {
     console.log("running main");
@@ -21,7 +24,16 @@ function StartScript(){
     plr = player;
     player.transform.position = new Vector2(0, 0);
     player.transform.scale = new Vector2(100, 100);
-    Engine.WindowSize = new Vector2(1400, 650);
+    Engine.WindowSize = new Vector2(800, 500);
+
+    let x = [];
+    for(let i = 0; i < 500; i++){
+        x.push(new Shape("circle", Color.Blue));
+    }
+
+    x.forEach(obj => {
+        obj.transform.scale = new Vector2(10, 10);
+    });
 
     let true_speed = 80;
     tru_spd = true_speed;
@@ -43,10 +55,21 @@ function StartScript(){
     // unity reference?
     function Start(){
         Engine.AddShape(player); 
+        x.forEach(obj => {
+        Engine.AddShape(obj);
+        });
     }
 
     // called every engine update at TargetFPS rate
     function Update(){
+        console.log("FPS: "+Math.round((1.0 / Engine.deltaTime)));
+        console.clear();
+        console.log("FPS: "+Math.round((1.0 / Engine.deltaTime)));
+
+        x.forEach(obj => {
+            obj.transform.position = new Vector2(Math.round(Math.random() * window.innerWidth), Math.round(Math.random() * window.innerHeight));
+        });
+
         speed = true_speed * Engine.deltaTime;
 
         if(keys["w"]){
@@ -64,11 +87,12 @@ function StartScript(){
     }
 
     // Add methods above into Engine for it to run
-    Engine.SetTargetFPS(50);
     Engine.AddStart(Start);
     Engine.AddUpdate(Update);
+
+    // Sets the target FPS for the engine and rendering to run at
+    Engine.SetTargetFPS(60);
 
     // Starts engine
     Engine.Start("canvas");
 }
-
