611c992da0dc302689216bdcfe77f381fb96f3a0
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-01 21:59:35 +0100

    Added loader.js to enigne

---
+++ EasyEngine/engine/loader.js
@@ -0,0 +1,24 @@
+async function async_main(){
+    // sleep
+    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+    // script directorys
+    const scripts = [
+        'engine/math.js',
+        'engine/conponents.js',
+        'engine/engine.js',
+        // Just sends 'engine-loaded' event out
+        'engine/event.js'
+    ];
+
+    // load the scripts
+    for(script of scripts){
+        const ref = document.createElement('script');
+        ref.src = script;
+        ref.defer = true;
+        document.head.appendChild(ref);
+        console.log("Injected script: " + script);
+        await sleep(10);
+    };
+}
+async_main();
