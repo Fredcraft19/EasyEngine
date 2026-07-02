6a38ba2df43ec7250a0a7dc3a57583149b85dba0
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-02 21:35:11 +0100

    Added PhysicBody Component

--- EasyEngine/engine/components.js
+++ EasyEngine/engine/components.js
@@ -1,26 +1,60 @@
+
 // The base game engine components
 
-window.Transform = class Transform{
+// ESSENTIAL CONPOENNTS
+class Component{
+    name = "BASE COMPONENT";
+    transform = null; // gameobject transform
+    gameObject = null;
+}
+
+window.Components = class Components{}
+
+Components.Transform = class Transform extends Component{
+    name = "Transform";
+    parent = null;
+
     position = new Vector2();
     scale = new Vector2();
+    rotation = 0;
 
-    SetPosition(x, y){
-        this.position.x = x;
-        this.position.y = y;
+    SetPosition(_x, _y){
+        if(this.gameObject.HasComponent("PhysicBody")){
+            physic = this.gameObject.GetComponent("PhysicBody");
+            
+            Matter.Body.setPosition(physic.body, {
+            x: _x,
+            y: _y
+        });
+        }
+        else{
+            this.position.x = _x;
+            this.position.y = _y;
+        }
+
     }
 
     SetScale(x, y){
         this.scale.x = x;
         this.scale.y = y;
     }
+    
+    SetParent(parent_transform){
+        this.parent = parent_transform;
+    }
 }
 
-window.Renderer = class Renderer{
+Components.Renderer = class Renderer extends Component{
+    name = "Renderer";
     color = null;
+
+    display = true;
+    
     type = "";
 
-    constructor(_color, _type){
-        this.color = _color
+    constructor(_color = new window.Color(255,0,0), _type = "rect"){
+        super();
+        this.color = _color || new window.Color.Red;
         this.type = _type;
     }
     
@@ -29,5 +63,61 @@ window.Renderer = class Renderer{
     }
     SetType(typ){
         this.type = typ;
+    }
+}
+
+// OPTIONAL CONPOENENTS
+
+const Matter = window.Matter;
+const MatterEngine = window.physicsEngine;
+window.physicsEngine.world.gravity.y = 250000;
+
+
+Components.PhysicBody = class PhysicBody extends Component{
+    name = "PhysicBody";
+    transform = null;
+    body = null;
+
+    friction = 10;;
+    solid = false;
+
+    velocity = new Vector2(0, 0);
+
+    Start(transform, type = "rect"){
+        this.transform = transform;
+
+        if(type == "circle"){ 
+            this.body = Matter.Bodies.circle( transform.position.x, transform.position.y, transform.scale.x);
+        }
+        else{ // Rectangle
+            this.body = Matter.Bodies.rectangle(transform.position.x, transform.position.y, transform.scale.x, transform.scale.y);
+        }
+
+        Matter.Composite.add(MatterEngine.world, this.body);
+
+        this.body.friction = this.friction;
+        Matter.Body.setStatic(this.body, this.solid);
+    }
+
+    Update(){
+        if (!this.body) return;
+
+        this.transform.position.x = this.body.position.x;
+        this.transform.position.y = this.body.position.y;
+        this.transform.rotation = this.body.angle;
+    }
+
+    AddForce(force){
+        Matter.Body.applyForce(this.body, this.body.position, {x: force.x, y: force.y});
+    }
+
+    GetVelocity(){
+        return new Vector2(this.body.velocity.x, this.body.velocity.y);
+    }
+    SetVelocity(newVelocity){
+        Matter.Body.setVelocity(this.body, {
+            x: newVelocity.x,
+            y: newVelocity.y
+        });
     }
 }
