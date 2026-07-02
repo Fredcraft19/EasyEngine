1877902e3fc88b69f2a92b2414ec9539e89f4807
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-02 20:01:53 +0100

    Same Velocity Example, new API

--- EasyEngine/code.js
+++ EasyEngine/code.js
@@ -6,39 +6,60 @@
 
 
 window.addEventListener("engine-loaded", () => {
-    console.log("running main");
+    console.log("Running Script : code.js");
     StartScript(); 
 });
 
 let plr = null;
+
+let box = null;
+
+let x_ref = null;
+
 let tru_spd = null;
-let fps = 0;
 let gravity = 9.8;
 let velocity = null;
-let friction = -0.01;
+let friction = -0.8;
 
 function StartScript(){
     
     // Import engine library
     const Engine = window.Engine;
-    const Shape = window.Shape;
+    const GameObject = window.GameObject;
     const Color = window.Color;
+    const Components = window.Components;
+
+    Engine.fullscreen = true;
+
+    box = new GameObject();
+    box.renderer.color = Color.Olive;
+    box.transform.SetPosition(200, 200);
+    box.transform.SetScale(50, 50);
+    box.AddComponent(new Components.PhysicBody());
     
     velocity = new Vector2(0, 0);
 
-    let player = new Shape("circle", Color.Red);
+    let player = new GameObject("player", "circle");
     plr = player;
+    player.AddComponent(new Components.PhysicBody());
+
+    player.renderer.color = Color.Green;
+    player.renderer.type = "circle";
     player.transform.position = new Vector2(0, 0);
     player.transform.scale = new Vector2(10, 100);
     Engine.WindowSize = new Vector2(800, 500);
 
     let x = [];
-    //for(let i = 0; i < 5000; i++){
-    //    col = new Color(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255), 25);
-    //    col = new Color(12, 155, 230);
-    //    col.a = 25;
-    //    x.push(new Shape("circle", col));
-    //}
+    x_ref = x;
+    
+    for(let i = 0; i < 1000; i++){
+        col = new Color(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255), 25);
+        col = new Color(12, 155, 230);
+        col.a = 25;
+        new_go = new GameObject(null, "circle");
+        new_go.renderer.color = col;
+        x.push(new_go);
+    }
 
     x.forEach(obj => {
         obj.transform.scale = new Vector2(10, 10);
@@ -64,18 +85,16 @@ function StartScript(){
     // called on engine start
     // unity reference?
     function Start(){
-        Engine.AddShape(player); 
         x.forEach(obj => {
-        Engine.AddShape(obj);
-        obj.transform.position.y = 0;
+        obj.transform.position.y = -10;
         obj.transform.position.x = Math.round(Math.random() * Engine.WindowSize.x);
         });
+        velocity = new Vector2(3, 3);
         
     }
 
     // called every engine update at TargetFPS rate
     function Update(){
-        fps = Math.round(1 / Engine.deltaTime);
         x.forEach(obj => {
             obj.transform.position.y += Math.round(Math.random() * 500) * Engine.deltaTime;
             if(obj.transform.position.y > Engine.WindowSize.y){
@@ -117,8 +136,9 @@ function StartScript(){
         if(player.transform.position.x > Engine.WindowSize.x){
             player.transform.position.x = velocity.x += 1;
         }
-        velocity.x = velocity.x + (velocity.x - 0) * friction;
-        velocity.y = velocity.y + (velocity.y - 0) * friction;
+
+        velocity.x = velocity.x + (velocity.x - 0) * (friction * Engine.deltaTime);
+        velocity.y = velocity.y + (velocity.y - 0) * (friction * Engine.deltaTime);
     }
 
     // Add methods above into Engine for it to run
@@ -126,7 +146,7 @@ function StartScript(){
     Engine.AddUpdate(Update);
 
     // Sets the target FPS for the engine and rendering to run at
-    Engine.SetTargetFPS(60);
+    Engine.SetTargetFPS(100);
 
     // Starts engine
     Engine.Start("canvas");
