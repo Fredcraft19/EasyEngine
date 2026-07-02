0ce923d35cdb03e53c56df67b2d6f54932b6825d
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-02 21:36:02 +0100

    Updated engine.js for physics and 'rotational rendering'
    by 'rotational rendering' i mean rendering objects that have a rotation

--- EasyEngine/engine/engine.js
+++ EasyEngine/engine/engine.js
@@ -1,65 +1,72 @@
-window.Shape = class Shape{
-    transform = null;
-    renderer = null;
-
-    // Add Layers later?
-
-    constructor(_type = "square", _color = "black"){
-        this.renderer = new window.Renderer(_color, _type);
-        this.transform = new window.Transform();
-    }
-}
-
-
 // Main engine code
 window.Engine = class Engine{
     /** @type {CanvasRenderingContext2D} */
     static #draw = null;
     static #canvas = null;
-
-    static deltaTime = 0.0;
-    static #targetFps = 60;
     static #starts = [];
     static #updates = [];
     static #interval = null;
     static #running = false;
 
-    static #shapes = [];
+    static #gameobjects = [];
+    static gameobjectCount = 0;
 
     static #oldTime = Date.now();
 
-    static isFullscreen = false;
+    static deltaTime = 0.0;
+    static fps = 0;
+    static #targetFps = 60;
+
+    static #framecount = 0;
+    static #frametime = 0;
+
+    static matter_js = window.physicsEngine;
+
+
+    static fullscreen = false;
     static WindowSize = new Vector2(800, 500);
+    static #memorySize = this.WindowSize;
+
+    static updates = this.#updates;
 
     // renders frame
     static #RenderFrame(){
-
-        if(this.isFullscreen){
-            this.#canvas.width = window.innerWidth;
-            this.#canvas.height = window.innerHeight;
-        }
-        else{
-            this.#canvas.width = this.WindowSize.x;
-            this.#canvas.height = this.WindowSize.y;
-        }
-
         this.#draw.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
         // Render
         this.deltaTime = (Date.now() - this.#oldTime) / 1000;
         this.#oldTime = Date.now();
+        this.#frametime += this.deltaTime;
+        this.#framecount += 1;
+        if(this.#frametime > 1){
+            this.#frametime = 0;
+            this.fps = this.#framecount;
+            this.#framecount = 0;
+        }
 
-        this.#shapes.forEach(shape => {
-            // for every shape in shapes[] list, render it.
-            this.#draw.fillStyle = shape.renderer.color.GetRGB();
+        this.#gameobjects.forEach(shape => {
+            // if there is no renderer. dont render. duh
+            if(shape.renderer != null && shape.renderer.display){
+                this.#draw.save();
+                // for every shape in shapes[] list, render it.
 
-            if(shape.renderer.type == "rectangle" || shape.renderer.type == "square" || shape.renderer.type == "rect"){
-                this.#draw.fillRect(shape.transform.position.x, shape.transform.position.y, shape.transform.scale.x, shape.transform.scale.y);
-            }
-            else if(shape.renderer.type == "circle"){
-                this.#draw.beginPath();
-                this.#draw.arc(shape.transform.position.x, shape.transform.position.y, shape.transform.scale.x, 0, 2 * Math.PI);
-                this.#draw.fill();  
-                this.#draw.closePath();
+                this.#draw.fillStyle = shape.renderer.color.GetRGB();
+
+                // radical!!?
+                let rad_ical = shape.transform.rotation * Math.PI / 180;
+
+                this.#draw.translate(shape.transform.position.x, shape.transform.position.y);
+                this.#draw.rotate(rad_ical);
+
+                if(shape.renderer.type == "rectangle" || shape.renderer.type == "square" || shape.renderer.type == "rect"){
+                    this.#draw.fillRect(-(shape.transform.scale.x / 2), -(shape.transform.scale.y / 2), shape.transform.scale.x, shape.transform.scale.y);
+                }
+                else if(shape.renderer.type == "circle"){
+                    this.#draw.beginPath();
+                    this.#draw.arc(-(shape.transform.scale.x / 2), -(shape.transform.scale.x / 2), shape.transform.scale.x, 0, 2 * Math.PI);
+                    this.#draw.fill();  
+                    this.#draw.closePath();
+                }
+                this.#draw.restore();
             }
             
         });
@@ -71,14 +78,13 @@ window.Engine = class Engine{
 
     static #Update(){
         // Every Engine update:
-        // Render FIRST
-
-        this.#RenderFrame();
-
-        // Run Update
         for(let x = 0; x < this.#updates.length; x++){
             this.#updates[x]();
         }
+
+        Matter.Engine.update(window.physicsEngine, this.deltaTime);
+
+        this.#RenderFrame();     
     }
 
     // sets target fps
@@ -94,17 +100,22 @@ window.Engine = class Engine{
 
     static AddStart(method){
         this.#starts.push(method);
+        if(this.#running){
+            method();
+        }
     }
 
     static AddUpdate(method){
         this.#updates.push(method);
     }
 
-    static AddShape(shape){
-        this.#shapes.push(shape);
+    static PushGameObject(shape){
+        this.gameobjectCount+=1;
+        this.#gameobjects.push(shape);
     }
-    static RemoveShape(shape){
-        this.#shapes.pop(shape);
+    static PopGameObject(shape){
+        this.gameobjectCount-=1;
+        this.#gameobjects.pop(shape);
     }
 
     static Start(canvas_id){
@@ -116,6 +127,21 @@ window.Engine = class Engine{
         // Get Canvas from ID
         this.#canvas = document.getElementById(canvas_id);
         this.#draw = this.#canvas.getContext('2d');
+
+        // Set canvas size
+        if(this.fullscreen){
+            this.#memorySize = this.WindowiSze
+            this.WindowSize.x = window.innerWidth;
+            this.WindowSize.y = window.innerHeight;
+            
+            this.#canvas.width = this.WindowSize.x;
+            this.#canvas.height = this.WindowSize.y;
+        }
+        else{
+            this.WindowSize = this.#memorySize;
+            this.#canvas.width = this.WindowSize.x;
+            this.#canvas.height = this.WindowSize.y;
+        }
 
         // Call all start functions
         for(let i = 0; i < this.#starts.length; i++){
