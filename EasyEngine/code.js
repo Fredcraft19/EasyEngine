a9805e9dc4ca972d03834d3a4af6f6b188237e1f
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-02 10:53:02 +0100

    Made a new Velocity Example

--- EasyEngine/code.js
+++ EasyEngine/code.js
@@ -13,6 +13,9 @@ window.addEventListener("engine-loaded", () => {
 let plr = null;
 let tru_spd = null;
 let fps = 0;
+let gravity = 9.8;
+let velocity = null;
+let friction = -0.01;
 
 function StartScript(){
     
@@ -21,19 +24,21 @@ function StartScript(){
     const Shape = window.Shape;
     const Color = window.Color;
     
-    let player = new Shape("rect", Color.Red);
+    velocity = new Vector2(0, 0);
+
+    let player = new Shape("circle", Color.Red);
     plr = player;
     player.transform.position = new Vector2(0, 0);
-    player.transform.scale = new Vector2(100, 100);
+    player.transform.scale = new Vector2(10, 100);
     Engine.WindowSize = new Vector2(800, 500);
 
     let x = [];
-    for(let i = 0; i < 5000; i++){
-        col = new Color(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255), 25);
-        col = new Color(12, 155, 230);
-        col.a = 25;
-        x.push(new Shape("circle", col));
-    }
+    //for(let i = 0; i < 5000; i++){
+    //    col = new Color(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255), 25);
+    //    col = new Color(12, 155, 230);
+    //    col.a = 25;
+    //    x.push(new Shape("circle", col));
+    //}
 
     x.forEach(obj => {
         obj.transform.scale = new Vector2(10, 10);
@@ -81,17 +86,39 @@ function StartScript(){
         speed = true_speed * Engine.deltaTime;
 
         if(keys["w"]){
-            player.transform.position.y -= speed;
+            velocity.y -= speed * Engine.deltaTime;  
         }
         if(keys["s"]){
-            player.transform.position.y += speed;
+            velocity.y += speed  * Engine.deltaTime; 
         }
         if(keys["a"]){
-            player.transform.position.x -= speed;
+            velocity.x -= speed * Engine.deltaTime;
         }
         if(keys["d"]){
-            player.transform.position.x += speed;
+            velocity.x += speed * Engine.deltaTime;
         }
+
+        //velocity.y += gravity * Engine.deltaTime * 0.1;
+
+        player.transform.position.x += velocity.x;
+        player.transform.position.y += velocity.y;
+
+
+        if(player.transform.position.y > Engine.WindowSize.y){
+            player.transform.position.y = velocity.y += 1;
+        }
+        if(player.transform.position.y < 0){
+            player.transform.position.y = Engine.WindowSize.y;
+        }
+
+        if(player.transform.position.x < 0){
+            player.transform.position.x = Engine.WindowSize.x;
+        }
+        if(player.transform.position.x > Engine.WindowSize.x){
+            player.transform.position.x = velocity.x += 1;
+        }
+        velocity.x = velocity.x + (velocity.x - 0) * friction;
+        velocity.y = velocity.y + (velocity.y - 0) * friction;
     }
 
     // Add methods above into Engine for it to run
