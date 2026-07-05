66f256f47a03c3a28f4ad121d348545bc83f8a8e
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-05 02:18:45 +0100

    Update editor.js for WIP inspector support

--- EasyEngine/ui/editor.js
+++ EasyEngine/ui/editor.js
@@ -1,15 +1,30 @@
-let fps = document.getElementById("fps_count");
+// if you see 'go' in a variable name, it probably means GameObject
 
+// Engine Performance Information
+let fps = document.getElementById("fps_count");
+// add drawcalls?
+
+// Engine Control
 let play = document.getElementById("play");
 let pause = document.getElementById("pause");
 
+// Hierarchy Varibles
 let hierarchy = document.getElementById("hierarchy");
-let hierarchy_objects = [];
+let go_count = -1;
 
-let go_count = 0;
+// Inspector Variables
+let inspector = document.getElementById("inspecter-components"); // 'An Inspector Calls' reference& ?
+let inspector_base = document.getElementById("inspector-base");
+inspector_base.style.display = "none";
+let go_target = null;
+let selected_id = -1;
+let ui_id = -1;
+
 
 window.addEventListener('engine-loaded', function () {
     console.log("Running editor.js");
+
+    
 
     document.getElementById("tt").innerText = "EasyEngine Editor " + Engine.version;
     document.getElementById("tv").innerText = "EasyEngine " + Engine.version;
@@ -26,6 +41,12 @@ window.addEventListener('engine-loaded', function () {
         Engine.SetTargetFPS(-1);
         Engine.deltaTime = 0;
     });
+
+    // Dis-select gameobject:
+    hierarchy.addEventListener('click', function () {
+        console.log("SELECTED ID = -1;");
+        selected_id = -1;
+    })
 
 
     // Update FPS
@@ -48,6 +69,10 @@ window.addEventListener('engine-loaded', function () {
             go_count = Engine.GameObjects.length;
         }
 
+        if (ui_id != selected_id) {
+            UpdateInspector();
+        }
+
     }, 100);
 
 });
@@ -67,7 +92,76 @@ function UpdateHierarchy() {
         div.addEventListener('click', ObjectClicked);
     });
 }
-
 function ObjectClicked(e) {
     console.log(e.currentTarget.id);
+}
+
+function UpdateInspector() {
+    ui_id = selected_id;
+    if (ui_id == -1) {
+        inspector.innerHTML = "";
+        inspector_base.style.display = "none";
+    }
+    target = null;
+
+    Engine.GameObjects.forEach(obj => {
+        if (obj.id == ui_id) {
+            target = obj;
+            console.log("target found!");
+        }
+    });
+
+    if (target == null) {
+        console.error(`Target not found of id '${ui_id}'`);
+        return;
+    }
+    // go found. get compoentns -> display components!
+    inspector_base.style.display = "block";
+
+    // foreach compoennt btw...
+    target.components.forEach(component => {
+    
+        CreateComponentField(component);
+
+    });   
+}
+function CreateComponentField(component){
+    let parent = document.createElement('div');
+    parent.className = "inspector-component";
+
+    let pparent = document.createElement('div');
+    pparent.className = "inspector-component2";
+
+    let comp_header = document.createElement('div');
+    comp_header.className = "component-header";
+
+
+    let img = document.createElement('img');
+    img.src = "ui/components/custom.png";
+    img.className = "mini-icon";
+
+    let comp_name = document.createElement('p');
+    comp_name.className = "no-margin";
+    comp_name.innerText = component.name;
+
+    comp_header.appendChild(img);
+    comp_header.appendChild(comp_name);
+    pparent.appendChild(comp_header);
+    // FOR EACH FIELD
+    let field = document.createElement('div');
+    field.className = "inspector-field";
+
+    let fLabel = document.createElement('p');
+    fLabel.className = "inspector-label";
+    fLabel.innerText = "Variable Name";
+    let fInput = document.createElement('input');
+    fInput.className = "inspector-input";
+    field.appendChild(fLabel);
+    field.appendChild(fInput);
+    pparent.appendChild(field);
+
+    // FOR EACH DONE
+
+    parent.appendChild(pparent);
+    inspector.appendChild(parent);
 }
