cbeed3c736f4afaedca09e37400ccac05e206c05
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-06 21:48:30 +0100

    Created Initial builder.js*
    *Its just empty methods for now

---
+++ EasyEngine/build/builder.js
@@ -0,0 +1,22 @@
+// bob the builder reference?
+// (can he fix it?)
+
+window.addEventListener('build-project', function(){
+
+});
+
+function GetData(){
+
+}
+
+function JSON(){
+
+}
+
+function GetCustomComponents{
+
+}
+
+function OuptutBuild(){
+
+}
